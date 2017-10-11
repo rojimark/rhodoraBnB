d8f6b5e494534d835a601ec6d89590a4cb664523
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-10-12 02:20:17 +0800

    swap photos

--- src/components/Rooms.vue
+++ src/components/Rooms.vue
@@ -7,6 +7,25 @@
  
 	<div class="row">
 
+
+<div class="col s12 m6">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="../assets/SampleRoom1.1.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Garden View Room<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Book this room</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Garden View Room<i class="material-icons right">close</i></span>
+      <p>Feast on a view of garden blooms and lush greens that cool your eyes and mind.</p>
+      <hr>
+      <p>Php x,xxx.xx /night – Good for 2 persons (maximum of 3 with extra mattress at Pxx/night)</p>
+    </div>
+  </div>
+</div>
+
     <div class="col s12 m6">
 	<div class="card">
     <div class="card-image waves-effect waves-block waves-light">
@@ -26,23 +45,7 @@
 </div>
 
 
-  <div class="col s12 m6">
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="../assets/SampleRoom1.1.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Garden View Room<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">Book this room</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Garden View Room<i class="material-icons right">close</i></span>
-      <p>Feast on a view of garden blooms and lush greens that cool your eyes and mind.</p>
-      <hr>
-      <p>Php x,xxx.xx /night – Good for 2 persons (maximum of 3 with extra mattress at Pxx/night)</p>
-    </div>
-  </div>
-</div>
+
 
 
  <div class="col s12 m6">
@@ -99,8 +102,6 @@
 </script>
 
 <style scoped>
-.roomintro{
-  padding:10px;
-}	
+
 
 </style>
